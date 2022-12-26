d8ab6440cfd9e9226f2a66904ddd7bf37e05fef7
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-12-26 21:40:40 +0200

    Updated_script.js

--- script.js
+++ script.js
@@ -16,4 +16,38 @@ function playetTwo(p2) {
     }
     return p2;
 }
+function p1Gmae(p1pick, p3) {
+    console.log(p1pick + " " + p3);
+    if (p1pick === "scissors" && p3 === "paper") {
+        p1score += 1;
+        document.getElementById("fin").innerHTML = "you won";
+    } else if (p1pick === "paper" && p3 === "rock") {
+        p1score += 1;
+        document.getElementById("fin").innerHTML = "you won";
+    } else if (p1pick === "rock" && p3 === "scissors") {
+        p1score += 1;
+        document.getElementById("fin").innerHTML = "you won";
+    } else if (p1pick === p3) {
+        document.getElementById("fin").innerHTML =
+            "No one won , please play again";
+    } else {
+        p2score += 1;
+        document.getElementById("fin").innerHTML = "Ai won";
+    }
+    console.log(p1score + " " + p2score);
+    document.getElementById("sc1").innerHTML = p1score;
+    document.getElementById("sc2").innerHTML = p2score;
+    trackScore.push(p1score);
+}
 
+function pick() {
+    p2pick = Math.floor(Math.random() * 3);
+    p3 = playetTwo(p2pick);
+}
+
+function p1Gmaee(atr) {
+    p1pick = atr;
+    p1Gmae(p1pick, p3);
+    pick();
+    console.log(trackScore.length);
+}
